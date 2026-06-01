bf8fac9ce6c4d00aee87f70853db089caa921dda
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-01 20:25:44 -0300

    Update ex01.py

--- aula5/ex01.py
+++ aula5/ex01.py
@@ -1,1 +1,17 @@
-ex01.py
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def comer(self):
+        print(f"{self.nome} está comendo")
+
+
+class Cachorro(Animal):
+    def latir(self):
+        print(f"{self.nome} está latindo")
+
+
+cachorro = Cachorro("Renan Santos")
+
+cachorro.comer()
+cachorro.latir()
