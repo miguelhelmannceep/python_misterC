11bcb653e1336c17ef2ac2bd1b86b6190c0c20ce
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-01 08:25:31 -0300

    Create py1.py

---
+++ aula2/py1.py
@@ -0,0 +1,6 @@
+dias_uteis = ("segunda", "terça", "quarta", "quinta", "sexta")
+
+print("Primeiro dia:", dias_uteis[0])
+print("Último dia:", dias_uteis[-1])
+
+print("Quantidade de dias:", len(dias_uteis))
