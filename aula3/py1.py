c81628f5d147e67ff13f2eba1eecf54e681feb30
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-01 08:30:07 -0300

    Update py1.py

--- aula3/py1.py
+++ aula3/py1.py
@@ -1,1 +1,16 @@
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
 
+
+produto1 = Produto("Camiseta MBL Prendeu Matou", 94.14)
+produto2 = Produto("Blusa MBL onça", 299.14)
+
+print("Produto 1:")
+print("Nome:", produto1.nome)
+print("Preço:", produto1.preco)
+
+print("\nProduto 2:")
+print("Nome:", produto2.nome)
+print("Preço:", produto2.preco)
