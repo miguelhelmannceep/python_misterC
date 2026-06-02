b9330d5873ae240c67878debd897e81a2f527062
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-02 10:25:20 -0300

    Update ex01.py

--- aula6/ex01.py
+++ aula6/ex01.py
@@ -1,1 +1,28 @@
-ex
+class Funcionario:
+    def calcular_salario(self):
+        return 0
+
+
+class Vendedor(Funcionario):
+    def __init__(self, salario_fixo, comissao):
+        self.salario_fixo = salario_fixo
+        self.comissao = comissao
+
+    def calcular_salario(self):
+        return self.salario_fixo + self.comissao
+
+
+class Gerente(Funcionario):
+    def __init__(self, salario_fixo, bonus):
+        self.salario_fixo = salario_fixo
+        self.bonus = bonus
+
+    def calcular_salario(self):
+        return self.salario_fixo + self.bonus
+
+
+vendedor = Vendedor(3000, 670)
+gerente = Gerente(6700, 2000)
+
+print("Salário do vendedor:", vendedor.calcular_salario())
+print("Salário do gerente:", gerente.calcular_salario())
