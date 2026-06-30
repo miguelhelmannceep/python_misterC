1088502b69cf301aaa39e3abf3f126267c7b04ef
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-29 21:41:51 -0300

    Update ex02.py

--- aulas/aula11/ex02.py
+++ aulas/aula11/ex02.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+conexao = sqlite3.connect("loja.db")
+conexao.row_factory = sqlite3.Row
+
+cursor = conexao.cursor()
+
+cursor.execute("SELECT * FROM produtos")
+
+produtos = cursor.fetchall()
+
+for produto in produtos:
+    print(dict(produto))
+
+conexao.close()
