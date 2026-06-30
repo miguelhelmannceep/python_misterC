693898374e0786bbf84695fcf264aefa4c264cc0
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-29 21:27:15 -0300

    Update ex02.py

--- aulas/aula9/ex02.py
+++ aulas/aula9/ex02.py
@@ -1,1 +1,24 @@
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+produtos = [
+    {"id": 1, "nome": "Camisa Chad", "preco": 12.0}
+]
+
+@app.route("/produtos", methods=["GET"])
+def listar():
+    return jsonify(produtos)
+
+# POST com validação
+@app.route("/produtos", methods=["POST"])
+def criar():
+    novo = request.get_json()
+
+    if "preco" not in novo:
+        return jsonify({"erro": "O preco eh obrigatorio"}), 400
+
+    produtos.append(novo)
+    return jsonify(novo), 201
+
+app.run(debug=True)
