926e0e33f6664cc28feed74aae92c31af3fcde0f
miguelhelmannceep <miguel.helmann@escola.pr.gov.br>
2026-06-29 21:41:17 -0300

    Update ex01.py

--- aulas/aula11/ex01.py
+++ aulas/aula11/ex01.py
@@ -1,1 +1,32 @@
+import sqlite3
 
+conexao = sqlite3.connect("loja.db")
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS produtos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    preco REAL
+)
+""")
+
+cursor.execute(
+    "INSERT INTO produtos (nome, preco) VALUES (?, ?)",
+    ("cuzcuzeira", 10.0)
+)
+
+cursor.execute(
+    "INSERT INTO produtos (nome, preco) VALUES (?, ?)",
+    ("chuteira nike pro max", 2000.0)
+)
+
+cursor.execute(
+    "INSERT INTO produtos (nome, preco) VALUES (?, ?)",
+    ("mouse super ruim", 1.0)
+)
+
+conexao.commit()
+conexao.close()
+
+print("Os produtos foram cadastrado!")
